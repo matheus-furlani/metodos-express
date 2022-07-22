9e5eb897ae600e72bef908fe73a352fbfc7bf23b
matheuscarretta <ext_matfurla@mercadolivre.com>
2022-07-22 11:34:37 -0300

    instala o express como dependencia

---
+++ produtos.js
@@ -0,0 +1,28 @@
+const produtos = [  
+  {
+    "name": "Fone de ouvido",
+    "price": 300.00,
+    "id": 1,
+    "description": "Fone de ouvido over-ear sem fio Philips 4000 Series TASH402 azul"
+  },
+  {
+    "name": "Kindle",
+    "price": 369.70,
+    "id": 2,
+    "description": "Kindle 10a. geração com bateria de longa duração - Cor Preta"
+  },
+  {
+    "name": "Notebook Lenovo IdeaPad",
+    "price": 597.55,
+    "id": 3,
+    "description": "5IML05 platinum gray 15.6, Intel Core i3 10110U 4GB de RAM 256GB SSD, Intel UHD Graphics 620 1366x768px Windows 11 Home"
+  },
+  {
+    "name": "Tv 43 polegadas Multilaser",
+    "price": 1700.43,
+    "id": 4,
+    "description": "Tela 43 Polegadas Full HD Sem Conversor Digital Multilaser HDMI+USB - TL023"
+  }
+] 
+
+module.exports = produtos;
