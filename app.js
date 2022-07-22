0ec49daa1a6afb1a6b3c67f015c888a2983c9036
matheuscarretta <ext_matfurla@mercadolivre.com>
2022-07-22 14:51:31 -0300

    cria as rotas de get product

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
+const produtos = require('./produtos');
+
 const app = express();
 
 app.get('/produtos', (req, res) => {
-  res.send('Que essa é a rota de produtos')
+  res.status(200).json(produtos);
+});
+
+app.get('/produtos/:id/:details?/:nome', (req, res) => {
+  const id = Number(req.params.id);
+
+  const details = req.params.details || 'Sem descrição';
+  console.log(details);
+
+  const nome = req.params.nome;
+  console.log(nome)
+
+  const product = produtos.find((produto) => produto.id === id)
+
+  res.status(200).json(product);
 })
 
 app.listen(3001, () => {
