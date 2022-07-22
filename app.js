0e8f84b6b515f59b448f7e2a2afd5cc4f2c50d5a
matheuscarretta <ext_matfurla@mercadolivre.com>
2022-07-22 16:18:11 -0300

    Cria a rota de put, post e delete

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 
-const produtos = require('./produtos');
+let produtos = require('./produtos');
 
 const app = express();
+
+app.post('/produtos1', (req, res) => {
+  const content = req.body;
+
+  const newProducts = [...produtos, content];
+
+  res.status(201).json(newProducts);
+})
+
+app.use(express.json());
 
 app.get('/produtos', (req, res) => {
   res.status(200).json(produtos);
 });
+
+app.post('/produtos', (req, res) => {
+  const content = req.body;
+
+  produtos = [...produtos, content];
+
+  res.status(201).json(produtos);
+})
 
 app.get('/produtos/:id/:details?/:nome', (req, res) => {
   const id = Number(req.params.id);
@@ -20,6 +38,41 @@ app.get('/produtos/:id/:details?/:nome', (req, res) => {
   const product = produtos.find((produto) => produto.id === id)
 
   res.status(200).json(product);
+})
+
+app.put('/produtos/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const content = req.body;
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  const produtoAtualizado = produtos.map((produto) => {
+    if (produto.id === id) return content;
+
+    return produto;
+  })
+
+  produtos = produtoAtualizado;
+
+  res.status(200).json(produtos);
+})
+
+app.delete('/produtos/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  produtos = produtos.filter((produto) => produto.id !== id );
+
+  res.status(200).json(produtos);
 })
 
 app.listen(3001, () => {
