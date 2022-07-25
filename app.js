dd6d8bad9c5818ef26212275a47049632810c82e
matheuscarretta <ext_matfurla@mercadolivre.com>
2022-07-25 14:38:50 -0300

    adiciona comentários ao código

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// A importação de express, do array de produtos e instanciação do express no app
 const express = require('express');
 
 let produtos = require('./produtos');
 
 const app = express();
 
+// Uma rota POST que não funciona, porque não identifica o body, precisa passar pelo express.json()
 app.post('/produtos1', (req, res) => {
   const content = req.body;
 
@@ -12,12 +14,15 @@ app.post('/produtos1', (req, res) => {
   res.status(201).json(newProducts);
 })
 
+// independente do método (POST/PUT), vai cair nessa rota que faz a rtadução do body
 app.use(express.json());
 
+// Vai pegar e responder com todos os produtos
 app.get('/produtos', (req, res) => {
   res.status(200).json(produtos);
 });
 
+// Adiciona um produto, esse funciona, pq passou pelo express.json()
 app.post('/produtos', (req, res) => {
   const content = req.body;
 
@@ -26,20 +31,20 @@ app.post('/produtos', (req, res) => {
   res.status(201).json(produtos);
 })
 
-app.get('/produtos/:id/:details?/:nome', (req, res) => {
+
+// Ele pega a informação de um único produto, e os paraâmetro de details é opcional
+app.get('/produtos/:id/:details?', (req, res) => {
   const id = Number(req.params.id);
 
   const details = req.params.details || 'Sem descrição';
   console.log(details);
-
-  const nome = req.params.nome;
-  console.log(nome)
 
   const product = produtos.find((produto) => produto.id === id)
 
   res.status(200).json(product);
 })
 
+// PUT faz a atualização de um objeto de acordo com o ID
 app.put('/produtos/:id', (req, res) => {
   const id = Number(req.params.id);
   const content = req.body;
@@ -61,6 +66,7 @@ app.put('/produtos/:id', (req, res) => {
   res.status(200).json(produtos);
 })
 
+// DELETE deleta o produto de id que seja o igual ao da parametro
 app.delete('/produtos/:id', (req, res) => {
   const id = Number(req.params.id);
 
@@ -72,7 +78,7 @@ app.delete('/produtos/:id', (req, res) => {
 
   produtos = produtos.filter((produto) => produto.id !== id );
 
-  res.status(200).json(produtos);
+  return res.status(200).json(produtos);
 })
 
 app.listen(3001, () => {
