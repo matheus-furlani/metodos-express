e8c1833a6b424251dce8ec4d0ea995bd73f46bb4
matheuscarretta <ext_matfurla@mercadolivre.com>
2022-07-25 16:00:05 -0300

    Adiciona rotas e alguns middlewares

--- app.js
+++ app.js
@@ -1,84 +1,23 @@
 // A importação de express, do array de produtos e instanciação do express no app
 const express = require('express');
 
-let produtos = require('./produtos');
-
 const app = express();
 
-// Uma rota POST que não funciona, porque não identifica o body, precisa passar pelo express.json()
-app.post('/produtos1', (req, res) => {
-  const content = req.body;
+const rotaProdutos = require('./routes/routeProdutos')
 
-  const newProducts = [...produtos, content];
-
-  res.status(201).json(newProducts);
-})
+const rotaUsuarios = require('./routes/routeUsuarios')
 
 // independente do método (POST/PUT), vai cair nessa rota que faz a rtadução do body
 app.use(express.json());
 
-// Vai pegar e responder com todos os produtos
-app.get('/produtos', (req, res) => {
-  res.status(200).json(produtos);
-});
+app.use('/produtos', rotaProdutos);
 
-// Adiciona um produto, esse funciona, pq passou pelo express.json()
-app.post('/produtos', (req, res) => {
-  const content = req.body;
+app.use('/user', rotaUsuarios);
 
-  produtos = [...produtos, content];
+app.use((req, res, next) => {
+  res.status(404).send('Erro 404, not found');
 
-  res.status(201).json(produtos);
-})
-
-
-// Ele pega a informação de um único produto, e os paraâmetro de details é opcional
-app.get('/produtos/:id/:details?', (req, res) => {
-  const id = Number(req.params.id);
-
-  const details = req.params.details || 'Sem descrição';
-  console.log(details);
-
-  const product = produtos.find((produto) => produto.id === id)
-
-  res.status(200).json(product);
-})
-
-// PUT faz a atualização de um objeto de acordo com o ID
-app.put('/produtos/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const content = req.body;
-
-  const product = produtos.find((produto) => produto.id === id);
-
-  if (!product) {
-    return res.status(400).json({ "message": "Produto não encontrado" })
-  }
-
-  const produtoAtualizado = produtos.map((produto) => {
-    if (produto.id === id) return content;
-
-    return produto;
-  })
-
-  produtos = produtoAtualizado;
-
-  res.status(200).json(produtos);
-})
-
-// DELETE deleta o produto de id que seja o igual ao da parametro
-app.delete('/produtos/:id', (req, res) => {
-  const id = Number(req.params.id);
-
-  const product = produtos.find((produto) => produto.id === id);
-
-  if (!product) {
-    return res.status(400).json({ "message": "Produto não encontrado" })
-  }
-
-  produtos = produtos.filter((produto) => produto.id !== id );
-
-  return res.status(200).json(produtos);
+  next();
 })
 
 app.listen(3001, () => {
---
+++ routes/routeProdutos.js
@@ -0,0 +1,116 @@
+const express = require('express');
+
+const routes = express.Router();
+
+let produtos = require('../produtos');
+
+// Uma rota POST que não funciona, porque não identifica o body, precisa passar pelo express.json()
+routes.post('/produtos1', (req, res) => {
+  const content = req.body;
+
+  const newProducts = [...produtos, content];
+
+  res.status(201).json(newProducts);
+})
+
+// Vai pegar e responder com todos os produtos
+routes.get('/', (req, res) => {
+  res.status(200).json(produtos);
+});
+
+function validatePrice (req, res, next) {
+  const { price } = req.body;
+
+  if (price && price >= 0) {
+    next();
+  }
+
+  return res.status(400).send('Produto com preço invalido')
+}
+
+function createUser(req, res, next) {
+  res.locals.user = {
+    nome: 'Matheus',
+    admin: false
+  }
+
+  next();
+  // res = {
+  //   prop: 1
+  //   nome:
+  //   local: {
+  //     user:
+  //   }
+  // }
+}
+
+function authUser ( req, res, next) {
+  const { user } = res.locals;
+  console.log(res.locals);
+  if(user.admin) {
+    next()
+  }
+
+  return res.status(403).send('Usuario não é admin')
+}
+
+// Adiciona um produto, esse funciona, pq passou pelo express.json()
+routes.post('/', createUser, authUser, (req, res) => {
+  const content = req.body;
+
+  produtos = [...produtos, content];
+
+  return res.status(201).json(produtos);
+})
+
+
+// Ele pega a informação de um único produto, e os paraâmetro de details é opcional
+routes.get('/:id/:details?', (req, res) => {
+  const id = Number(req.params.id);
+
+  const details = req.params.details || 'Sem descrição';
+  console.log(details);
+
+  const product = produtos.find((produto) => produto.id === id)
+
+  res.status(200).json(product);
+})
+
+// PUT faz a atualização de um objeto de acordo com o ID
+routes.put('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const content = req.body;
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  const produtoAtualizado = produtos.map((produto) => {
+    if (produto.id === id) return content;
+
+    return produto;
+  })
+
+  produtos = produtoAtualizado;
+
+  res.status(200).json(produtos);
+})
+
+// DELETE deleta o produto de id que seja o igual ao da parametro
+routes.delete('/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  produtos = produtos.filter((produto) => produto.id !== id );
+
+  return res.status(200).json(produtos);
+})
+
+module.exports = routes;
